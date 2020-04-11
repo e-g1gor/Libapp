99658b0ab9f9c0fa40469985dbc8761af36a49bf
Egor Klenin <e.g2gor@gmail.com>
2020-04-11 17:01:01 +0300

    GUI

--- public/app.js
+++ public/app.js
@@ -1,3 +1,41 @@
 'use strict'
 
-document.title='Libapp: code loaded'
+
+// Show loaded books
+function render(data, error) {
+  if (error) {
+    output.innerHTML = JSON.stringify(data)
+    return
+  }
+  let output = document.getElementById('output')
+  let rez = `<table>${new Array(...data).map(x => "<tr><td>" + x.name + "</td></tr>").join("\n")}<table>`
+  output.innerHTML = rez
+
+}
+
+
+// Load books list, show result
+function getBooks(lang) {
+  fetch(lang === 'all' ? '/books' : '/books/lang/' + lang)
+    .then(response => response.json())
+    .then(render)
+    .catch(err => render(err, false))
+}
+
+
+// ENTRY POINT
+window.onload = () => {
+  // Get all books
+  getBooks('all')
+
+  // If language changed, load new list
+  document
+    .getElementById('selectlang')
+    .addEventListener('change', (e) => {
+      // Show loading message
+      render('Загрузка...')
+      // Get data
+      let lang = e.target[e.target.selectedIndex].value.replace(/[^a-z]/igm, '')
+      getBooks(lang)
+    })
+}
